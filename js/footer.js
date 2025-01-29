e318d6dc5b4e8eb2b3f8112bcebae1612eecd466
MarcosCGoenaga <mcostapricesmart@gmail.com>
2025-01-29 13:50:10 -0500

    Create footer.js

---
+++ js/footer.js
@@ -0,0 +1,8 @@
+// js/footer.js
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.insertAdjacentHTML('beforeend', `
+        <footer class="bg-primary text-white text-center py-3" style="position: fixed; width: 100%; bottom: 0;">
+            <p>&copy; 2025 Marcos Costa. All rights reserved.</p>
+        </footer>
+    `);
+});
