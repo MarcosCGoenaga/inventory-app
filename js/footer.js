490513ec642941d48c45103b1073b066af6b9bf4
MarcosCGoenaga <mcostapricesmart@gmail.com>
2025-01-29 16:35:16 -0500

    Update and rename footer.js to layout.js

--- js/footer.js
+++
@@ -1,8 +0,0 @@
-// js/footer.js
-document.addEventListener("DOMContentLoaded", function() {
-    document.body.insertAdjacentHTML('beforeend', `
-        <footer class="bg-light text-dark text-center py-3" style="position: fixed; width: 100%; bottom: 0; border-top: 1px solid #dee2e6;">
-            <p>&copy; 2025 Marcos Costa. All rights reserved.</p>
-        </footer>
-    `);
-});
