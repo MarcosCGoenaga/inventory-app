cc0c4257955d0fdfc5becc3c353c734c31b9e593
MarcosCGoenaga <mcostapricesmart@gmail.com>
2025-01-29 13:11:18 -0500

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,24 @@
-// navbar.js
+// js/navbar.js
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar-placeholder').innerHTML = data;
-        });
+    document.getElementById("navbar-placeholder").innerHTML = `
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="index.html">Inventory Tracker</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="ppl.html">PPL</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="product-form.html">Add Product</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    `;
 });
