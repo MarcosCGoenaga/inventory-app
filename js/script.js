8f23b62ec6a4681432be00ca35a36629634d882d
MarcosCGoenaga <mcostapricesmart@gmail.com>
2025-01-29 11:14:09 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -80,3 +80,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     renderProductList();
 });
+// navbar.js
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-placeholder').innerHTML = data;
+        });
+});
