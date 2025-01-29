3f868635e3b9696b00d68b5b2022079e81e3afd6
MarcosCGoenaga <mcostapricesmart@gmail.com>
2025-01-29 11:45:20 -0500

    Delete navbar.js

--- navbar.js
+++
@@ -1,8 +0,0 @@
-// navbar.js
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar-placeholder').innerHTML = data;
-        });
-});
