ba66f4a1c91c78e34bd739cd1b64525cafd8e992
Marcos <mcostapricesmart@gmail.com>
2025-01-30 09:02:57 -0500

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Load navbar
   fetch("navbar.html")
     .then(response => response.text())
     .then(data => document.getElementById("navbar-placeholder").innerHTML = data)
     .catch(error => console.error("Error loading navbar:", error));
+
+  // Load footer
+  fetch("footer.html")
+    .then(response => response.text())
+    .then(data => document.getElementById("footer-placeholder").innerHTML = data)
+    .catch(error => console.error("Error loading footer:", error));
 });
